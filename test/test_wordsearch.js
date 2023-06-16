e2d12f86f3c824ad9263ad611581db50fde6f345
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-16 11:36:48 -0700

    Finished pair programming with the collaboration of @carolineolagunju

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,54 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'S', 'A', 'L'],
+      ['X', 'X', 'X', 'X', 'X', 'E', 'X', 'X'],
+      ['Y', 'F', 'C', 'F', 'Q', 'I', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'N', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'F', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'L', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'D', 'A', 'S'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the array contains empty arrays", function() {
+    const result = wordSearch([
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+    ], 'SEINFELD')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([], 'SEINFELD')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if a string is passed as a paramater", function() {
+    const result = wordSearch("testing!", 'SEINFELD')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if a number is passed as a paramater", function() {
+    const result = wordSearch(123456, 'SEINFELD')
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,51 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// Function from the previous pair programming exercise.
+// It swaps the matrix's columns for its rows.
+const transpose = (matrix) => {
+    const result = [];
+  
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        //make sure there is an array in place. If not, create one
+        if (!Array.isArray(result[j])) {
+          result[j] = [];
+        }
+    
+        //add the element switching i and j (row for column)
+        //put matrix[i][j] element in result[j][i]
+        result[j][i] = matrix[i][j];
+      }
     }
-}
+  
+    return result;
+  };
+  
+const wordSearch = (letters, word) => { 
+    //letters should be an Array
+    if (!Array.isArray(letters)) {
+        return (false);
+    }
+
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+    //Horizontal search
+    if(horizontalSearch(horizontalJoin, word)) {
+        return (true);
+    }
+    
+    //Vertical search
+    const verticalJoin = transpose(horizontalJoin).map(ls => ls.join(''));
+
+    return (horizontalSearch(verticalJoin, word));    
+};
+
+const horizontalSearch = (matrix, value) => {
+    for (const item of matrix) {
+        if (item.includes(value)) {
+            return (true);
+        } 
+    }
+
+    return (false);
+};
 
 module.exports = wordSearch
